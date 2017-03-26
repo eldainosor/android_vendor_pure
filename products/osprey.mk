026e2bf23abaf8e8c640a90e3441ecf858a933fd
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-03-25 23:08:29 -0300

    Remove kenzo and add osprey

--- products/osprey.mk
+++ products/osprey.mk
@@ -15,28 +15,17 @@
 # Include pure telephony configuration
 include vendor/pure/configs/pure_phone.mk
 
-# Inherit AOSP device configuration for kenzo
-$(call inherit-product, device/xiaomi/kenzo/aosp_kenzo.mk)
+# Inherit AOSP device configuration for osprey
+$(call inherit-product, device/motorola/osprey/aosp_osprey.mk)
 
 ifeq ($(WITH_GAPPS),true)
-# Inherit arm64 phone gapps
-$(call inherit-product-if-exists, vendor/gapps/arm64-phone-gapps.mk)
+# Inherit arm phone gapps
+$(call inherit-product-if-exists, vendor/gapps/arm-phone-gapps.mk)
 endif
 
 # Set those variables here to overwrite the inherited values.
-BOARD_VENDOR := Xiaomi
-PRODUCT_BRAND := Xiaomi
-PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := kenzo
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Redmi Note 3
-TARGET_VENDOR := Xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Use the latest approved GMS identifiers unless running a signed build
-ifneq ($(SIGN_BUILD),true)
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V8.1.6.0.MHOMIDI:user/release-keys \
-    PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M V8.1.6.0.MHOMIDI release-keys"
-endif
+PRODUCT_DEVICE := osprey
+PRODUCT_NAME := osprey
+PRODUCT_BRAND := Motorola
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := osprey
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo kenzo-userdebug
+add_lunch_combo osprey-userdebug
